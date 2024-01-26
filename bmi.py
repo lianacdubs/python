d65e2da6629387852067f09ed13f7b0e9e001991
Liana Williams <139722683+lianacdubs@users.noreply.github.com>
2024-01-25 16:34:22 -0800

    Delete bmi.py

--- bmi.py
+++
@@ -1,8 +0,0 @@
-#Homework 1 Problem 5 bmi.py
-#Liana Williams
-#
-#Calculate BMI given the inputs of height in inches and weight in pounds by translating the CDC formula into Python.
-height= float(input("Enter height in inches:"))
-weight= float(input("Enter weight in pounds:"))
-bmi= weight/((height)**2)*703 #formula for BMI
-print("BMI:", bmi)
